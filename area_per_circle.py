350bc32c58309b6f1af7e980e762f1962012d731
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-21 22:26:39 +0000

    fixed some style stuff

--- area_per_circle.py
+++ area_per_circle.py
@@ -10,8 +10,8 @@ import math
 
 def main():
     print("For a circle with a radius of 15mm:")
-    print("Area of the circle = {}mm^2".format(math.pi*15**2))
-    print("Perimeter of the circle = {}mm".format(2*math.pi*15))
+    print("Area of the circle = {}mm^2".format(math.pi * 15**2))
+    print("Perimeter of the circle = {}mm".format(2 * math.pi * 15))
 
 
 if __name__ == "__main__":
